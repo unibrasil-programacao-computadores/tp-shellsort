1a71df0deecfcc27f4cecc5d7cd8218ec53de312
matheus777-P <matheusspereira9@hotmail.com>
2025-06-17 18:44:56 -0300

    Ajuste de erros

--- sort/sort.c
+++ sort/sort.c
@@ -12,32 +12,33 @@ void quicksort(double* arr, long arr_size)
 }
 
 void shellsort(double* arr, long arr_size) {
-    int i, j, h;
+    int i, j, salto;
     double aux;
-
-    float k = log(arr_size + 1) / log(3);
-    k = floor(k + 0.5);
-    h = (pow(3, k) - 1) / 2;
-
-    while (h > 0) {
-        for (i = 0; i < arr_size - h; i++) {
-            if (arr[i] > arr[i + h]) {
-                aux = arr[i + h];
-                arr[i + h] = arr[i];
+    //Calculo de knuth
+    float potencia = log(arr_size + 1) / log(3);
+    potencia = floor(potencia + 0.5);
+    salto = (pow(3, potencia) - 1) / 2;
+    //Inicio ShellSort
+    while (salto > 0) {
+        for (i = 0; i < arr_size - salto; i++) {
+            if (arr[i] > arr[i + salto]) {
+                aux = arr[i + salto];
+                arr[i + salto] = arr[i];
                 arr[i] = aux;
-                j = i - h;
+                j = i - salto;
 
                 while (j >= 0) {
                     if (aux < arr[j]) {
-                        arr[j + h] = arr[j];
+                        arr[j + salto] = arr[j];
                         arr[j] = aux;
                     } else {
                         break;
                     }
-                    j = j - h;
+                    j = j - salto;
                 }
             }
         }
-        h = (h - 1) / 3;
+        //Inverca de knuth
+        salto = (salto - 1) / 3;
     }
 }
